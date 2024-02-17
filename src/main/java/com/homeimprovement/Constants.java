831a4b4a20bd19585b1fc41744a5bc8426be308d
Mike-U5 <maikel.haarmans2@gmail.com>
2024-02-17 11:16:01 +0100

    Add West of Hosidius Estate Agent and South Varrock to Constants

--- src/main/java/com/homeimprovement/Constants.java
+++ src/main/java/com/homeimprovement/Constants.java
@@ -7,16 +7,29 @@ import java.util.Map;
 public class Constants {
 	public static final int BUILD_MODE_VARBIT = 2176;
 	public static final List<String> POH_OPTIONS = List.of("Remove" , "Upgrade" , "Revert" , "Remove-room" , "Remove-decorations" , "Build-in", "Build");
-	// This array has all Mahogany Homes objects and the morph ID they have when they are in need of replacement
 	public static final List<Integer> POH_DUNGEON_DOOR_HOTSPOT = List.of(15317, 37475);
 	public static final int POH_PORTAL_ID = 4525;
 	public static final int WINTUMBER_TREE_ID = 19038;
+	// This array has all Mahogany Homes objects and the morph ID they have when they are in need of replacement
 	public static final Map<Integer, Integer> MH_OBJECTS = new HashMap<>() {{
+		// West of Hosidius Estate Agent
+		put(40002, 40017);
+		put(40003, 40025);
+		put(40004, 40043);
+		put(40005, 40031);
+		put(40006, 40031);
+		put(40288, 40037);
 		// East of Hosidius Market
 		put(40007, 40031);
 		put(40008, 40031);
 		put(40009, 40017);
 		put(40291, 40050);
+		// South of Hosidius
+		put(40012, 40017);
+		put(40013, 40043);
+		put(40014, 40062);
+		put(40015, 40062);
+		put(40294, 40056);
 		// North of Falador fountain
 		put(40095, 40151);
 		put(40096, 40151);
@@ -34,12 +47,6 @@ public class Constants {
 		put(40092, 40134);
 		put(40093, 40143);
 		put(40094, 39967);
-		// South of Hosidius
-		put(40012, 40017);
-		put(40013, 40043);
-		put(40014, 40062);
-		put(40015, 40062);
-		put(40294, 40056);
 		// North of Ardougne Church
 		put(40165, 40187);
 		put(40166, 40187);
@@ -74,7 +81,11 @@ public class Constants {
 		put(39993, 39923);
 		put(39994, 39948);
 		put(39996, 39955);
-		//
-		put(-42, 0);
+		// South Varrock
+		put(39997, 39961);
+		put(39998, 39974);
+		put(39999, 39948);
+		put(40000, 39967);
+		put(40001, 39935);
 	}};
 }
